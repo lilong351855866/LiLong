42dc0967ae830d605cf6f95d82785b16110e076f
LiLong <351855866@qq.com>
2026-08-05 14:15:21 +0800

    小程序修改、添加关于小程序

--- 开发/DragonSpot/pages/mine/mine.js
+++ 开发/DragonSpot/pages/mine/mine.js
@@ -1,8 +1,27 @@
 Page({
+  data:{
+    showModal:false,
+    showAbout:false
+  },
   noFunc() {
     wx.showToast({
       title: "功能暂未开放",
       icon: "none"
     })
-  }
+  },
+  //打开关于弹窗
+  showAboutModal(){
+    this.setData({showAbout:true})
+  },
+  closeAboutModal(){
+    this.setData({showAbout:false})
+  },
+  //联系我弹窗
+  showContact(){
+    this.setData({showModal:true})
+  },
+  closeModal(){
+    this.setData({showModal:false})
+  },
+  stopEvent(){}
 })
